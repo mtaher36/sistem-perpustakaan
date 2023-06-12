2a54a4506c8fcc7378d8079e4ecf9f703e813008
mtaher36 <mtaher3331@gmail.com>
2023-06-12 20:22:04 +0700

    my changes

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -38,7 +38,7 @@ class Email extends BaseConfig
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'akuganteng612';
+    public string $SMTPPass = '';
 
     /**
      * SMTP Port
